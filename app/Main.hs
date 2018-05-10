ac8c70e6e5e82f6c76411f945728067d3b0b2de2
Brad Chase <brad@these8bits.com>
2018-05-10 18:58:55 -0400

    rearrange main to show `Stripe` logic first

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,23 @@ import           Stripe
 
 
 
+createAndChargeAndDeleteCustomer :: Stripe (Customer, Charge, [Charge], Bool)
+createAndChargeAndDeleteCustomer = do
+  cust    <- stripeS WithoutConnect . createCustomer $ custReq
+  charge  <- stripeS WithoutConnect . createCharge   $ chargeReq cust
+
+  charges <- stripeL WithoutConnect . paginate [ By 10 ] $ listCharges
+
+  deleted <- fmap stripeDestroyDeleted . stripeD' WithoutConnect . destroyCustomer $ customerId cust
+
+  return (cust, charge, charges, True)
+
+  where
+    custReq = (customerCreateReq (Token "tok_visa")) { customerCreateEmail = Just "test@example.com" }
+
+    chargeReq Customer{customerId} = chargeCreateReq (Price USD 10000) (PCustomer customerId)
+
+
 main :: IO ()
 main = do
   let ver    = StripeVersion'2017'08'15
@@ -16,24 +33,7 @@ main = do
   eResp <- stripeIO config createAndChargeAndDeleteCustomer
 
   case eResp of
-    Right (cust, charge, charges, gone) -> print charge >> print gone
     Left  (StripeErrorResponse   err  ) -> putStrLn "Stripe Error:"     >> print err
     Left  (StripeDecodeFailure   err _) -> putStrLn "Decode Failure:"   >> print err
     Left  (StripeConnectionError err  ) -> putStrLn "Connection Error:" >> print err
-
-
-createAndChargeAndDeleteCustomer :: Stripe (Customer, Charge, [Charge], Bool)
-createAndChargeAndDeleteCustomer = do
-  cust    <- stripeS WithoutConnect . createCustomer $ custReq
-  charge  <- stripeS WithoutConnect . createCharge   $ chargeReq cust
-
-  charges <- stripeL WithoutConnect . paginate [ By 10 ] $ listCharges
-
-  deleted <- stripeDestroyDeleted <$> (stripeD' WithoutConnect . destroyCustomer $ customerId cust)
-
-  return (cust, charge, charges, True)
-
-  where
-    custReq = (customerCreateReq (Token "tok_visa")) { customerCreateEmail = Just "test@example.com" }
-
-    chargeReq Customer{customerId} = chargeCreateReq (Price USD 10000) (PCustomer customerId)
+    Right (cust, charge, charges, gone) -> print charge
